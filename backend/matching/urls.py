1c5620616a2d76df324c9ae10d94a8da6546b656
ValuteMaster413 <134705162+ValuteMaster413@users.noreply.github.com>
2025-05-19 18:11:27 +0300

    Likes for matching

--- backend/matching/urls.py
+++ backend/matching/urls.py
@@ -3,4 +3,5 @@ from .views import find_matches
 
 urlpatterns = [
     path('match/<int:pet_id>/', find_matches, name='find_matches'),
+    path('match/<int:pet_id>/like/', find_matches, name='like'),
 ]
--- backend/matching/views.py
+++ backend/matching/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from pets.models import Pet
+from .models import Like, Sympathy
+from django.db.models import Q
 
 def find_matches(request, pet_id):
     pet = get_object_or_404(Pet, id=pet_id)
@@ -17,9 +19,6 @@ def find_matches(request, pet_id):
     if pet.breed:
         matches = matches.filter(breed=pet.breed)
 
-    if pet.coat_color:
-        matches = matches.filter(coat_color=pet.coat_color)
-
     if pet.price:
         matches = matches.filter(price__lte=pet.price)
 
@@ -29,9 +28,84 @@ def find_matches(request, pet_id):
         "gender": match.gender,
         "breed": match.breed,
         "price": match.price,
-        "coat_color": match.coat_color,
         "age": match.age,
         "photos": [photo.image.url for photo in match.photo_set.all()]
     } for match in matches]
 
     return JsonResponse({"matches": results})
+
+def like(request, pet_id_like_from,pet_id_like_to):
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User not authenticated'}, status=401)
+        
+        pet_from = get_object_or_404(Pet, id=pet_id_like_from)
+        pet_to = get_object_or_404(Pet, id=pet_id_like_to)
+
+        if Like.objects.filter(petLikeFrom=pet_from, petLikeTo=pet_to).exists():
+            return JsonResponse({'success': False, 'message': 'Like already exists'})
+
+        like = Like.objects.create(petLikeFrom=pet_from, petLikeTo=pet_to)
+
+        like.save()
+
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+    
+def likes_to_me(request, pet_id):
+    if request.method == "GET":
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User not authenticated'}, status=401)
+        
+        likes = Like.objects.filter(petLikeTo = pet_id)
+        data = [{'from': like.petLikeFrom.id} for like in likes]
+
+        return JsonResponse({"likes_to_me": data})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+    
+def likes_from_me(request, pet_id):
+    if request.method == "GET":
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User not authenticated'}, status=401)
+        
+        likes = Like.objects.filter(petLikeFrom = pet_id)
+        data = [{'to': like.petLikeTo.id} for like in likes]
+
+        return JsonResponse({"likes_from_me": data})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+    
+def get_sympathy(request, pet_id):
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User not authenticated'}, status=401)
+
+        pet = get_object_or_404(Pet, id=pet_id)
+
+        likes_from = Like.objects.filter(petLikeFrom=pet)
+
+        for like in likes_from:
+            if Like.objects.filter(petLikeFrom=like.petLikeTo, petLikeTo=pet).exists():
+                pet1, pet2 = sorted([pet.id, like.petLikeTo.id])
+                if not Sympathy.objects.filter(pet1_id=pet1, pet2_id=pet2).exists():
+                    Sympathy.objects.create(pet1_id=pet1, pet2_id=pet2)
+
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+    
+def get_all_sympathys(request, pet_id):
+    if request.method == "GET":
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User not authenticated'}, status=401)
+
+        sympathys = Sympathy.objects.filter(Q(pet1_id=pet_id) | Q(pet2_id=pet_id))
+        
+        data = [{'pet1': s.pet1.id, 'pet2': s.pet2.id} for s in sympathys]
+
+        return JsonResponse({"sympathys": data})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
